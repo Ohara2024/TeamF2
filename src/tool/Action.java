4f3dde8ddb6e7038bdf998b5750254cf13a37400
admin <admin@LAPTOP-FRBSDC3F>
2025-05-21 14:20:12 +0900

    fix : master

--- src/tool/Action.java
+++
@@ -1,8 +0,0 @@
-package tool;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface Action {
-    void execute(HttpServletRequest req, HttpServletResponse res) throws Exception;
-}
